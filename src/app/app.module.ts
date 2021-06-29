6dc206dc261932f4330d2c16b0703ec697e9c059
Abdul Khan <akhan17@outlook.com>
2021-06-29 18:00:40 -0500

    added button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SuccComponent } from './buttonComp/succ/succ.component';
 import { FailComponent } from './buttonComp/fail/fail.component';
 import { NeutralComponent } from './neutral/neutral/neutral.component';
+import { RandomButtonComponent } from './buttonComp/random-button/random-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SuccComponent,
     FailComponent,
-    NeutralComponent
+    NeutralComponent,
+    RandomButtonComponent
   ],
   imports: [
     BrowserModule
