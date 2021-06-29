a6dd89932b22aedce3fd014f283bc011339cce4f
Abdul Khan <akhan17@outlook.com>
2021-06-28 19:11:51 -0500

    created and tested components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccComponent } from './buttonComp/succ/succ.component';
+import { FailComponent } from './buttonComp/fail/fail.component';
+import { NeutralComponent } from './neutral/neutral/neutral.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccComponent,
+    FailComponent,
+    NeutralComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,SuccComponent]
 })
 export class AppModule { }
